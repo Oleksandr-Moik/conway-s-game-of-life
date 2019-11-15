bb185b48324b24c14d726734f5c0cd3a388857a5
Virus_Mix <Virus_Mix@users.noreply.github.com>
2019-11-15 21:18:38 +0200

    created files class and interface

---
+++ Game_of_life/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_life
+{
+    public partial class MainWindow : Form
+    {
+
+        private Graphics graphics;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen blackPen = new Pen(Color.Black, 2);
+            graphics.DrawLine(blackPen, 20, 20, 200, 100);
+
+            graphics.Dispose();
+        }
+    }
+}
